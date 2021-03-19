5d0385f454aebfed0b1a83fd3fc342c9838f14b3
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-03-19 19:21:10 -0400

    linked up products to back end instead of directly on front end

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
+import axios from 'axios';
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = ({match}) => {
-	const product = products.find((p) => p._id === match.params.id);
+	const id = match.params.id;
+	const [product, setProduct] = useState([]);
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const {data} = await axios.get(`/api/products/${id}`);
+			setProduct(data);
+		};
+		fetchProduct();
+	}, [id]);
+	// const product = products.find((p) => p._id === match.params.id);
 
 	return (
 		<>
