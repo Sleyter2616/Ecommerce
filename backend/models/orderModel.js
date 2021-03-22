0fb7a8c0a9d578a007ed621b283af30ffe353086
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-03-22 18:04:09 -0400

    seeded data into the DB, Fetched from DB for products using routes and created error handlers that creates all json data

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const orderSchema = mongoose.Schema(
 	{
@@ -52,7 +52,7 @@ const orderSchema = mongoose.Schema(
 			default: 0.0,
 		},
 		isPaid: {
-			type: boolean,
+			type: Boolean,
 			required: true,
 			default: false,
 		},
@@ -60,8 +60,9 @@ const orderSchema = mongoose.Schema(
 			type: Date,
 		},
 		isDelivered: {
-			type: boolean,
+			type: Boolean,
 			required: true,
+			default: false,
 		},
 		deliveredAt: {
 			type: Date,
@@ -70,8 +71,8 @@ const orderSchema = mongoose.Schema(
 	{
 		timestamps: true,
 	}
-);
+)
 
-const Order = mongoose.model('Order', orderSchema);
+const Order = mongoose.model('Order', orderSchema)
 
-export default User;
+export default Order
