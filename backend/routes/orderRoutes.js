5e1f50dc23c92c4504f6f71f2d4394c085d4c29a
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-04-06 22:13:28 -0400

    added order and sent it over to the server and by extension the database. pushes to next step

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {protect} from '../middleware/authMiddleware.js'
-import {addOrderItems, orderItems} from '../controllers/orderController.js'
+import {addOrderItems} from '../controllers/orderController.js'
 const router = express.Router()
 
 //Functionality is in controllers
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,7 @@ import {
 	userDetailsReducer,
 	userUpdateReducer,
 } from './reducers/userReducers'
+import {orderCreateReducer} from './reducers/orderReducers'
 
 const reducer = combineReducers({
 	productList: productListReducer,
@@ -21,6 +22,7 @@ const reducer = combineReducers({
 	userRegister: userRegisterReducer,
 	userDetails: userDetailsReducer,
 	userUpdate: userUpdateReducer,
+	orderCreate: orderCreateReducer,
 })
 const cartItemsFromStorgage = localStorage.getItem('cartItems')
 	? JSON.parse(localStorage.getItem('cartItems'))
