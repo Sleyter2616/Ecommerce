ffebca3d7e47edf3d9a20e769bf696b21f991daf
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-04-07 09:15:26 -0400

    able to get order by Id from database

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import {protect} from '../middleware/authMiddleware.js'
-import {addOrderItems} from '../controllers/orderController.js'
+import {addOrderItems, getOrderById} from '../controllers/orderController.js'
 const router = express.Router()
 
 //Functionality is in controllers
 router.route('/').post(protect, addOrderItems)
-
+router.route('/:id').get(protect, getOrderById)
 export default router
