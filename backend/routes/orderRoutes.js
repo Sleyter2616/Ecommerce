799a298ed9beda18f5f3421bdf0c377312c91f98
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-04-15 08:36:35 -0400

    added get orders for admin

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express'
-import {protect} from '../middleware/authMiddleware.js'
+import {protect, admin} from '../middleware/authMiddleware.js'
 import {
 	addOrderItems,
 	getOrderById,
 	updateOrderToPaid,
 	getMyOrders,
+	getOrders,
 } from '../controllers/orderController.js'
 const router = express.Router()
 
 //Functionality is in controllers
-router.route('/').post(protect, addOrderItems)
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
